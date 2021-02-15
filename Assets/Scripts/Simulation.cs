673bf6dfb65fe6213c1e3d7ba50984b781d6d808
Rylan Marianchuk <rymarianchuk@outlook.com>
2021-02-15 17:55:28 -0600

    File Structure created

---
+++ Assets/Scripts/Simulation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simulation : MonoBehaviour
+{
+    // Parameters
+
+    void Start()
+    {
+        // Initialize agents & documents
+        // Randomly assign agent parameters
+
+    }
+
+    void FixedUpdate()
+    {
+        // Wait
+        // For each agent, choose a document or choose to post - add to recommender pool - RecSim corpus
+
+        // Wait
+        // Move agent after consumption Vector3 position
+
+        // Wait
+        // Recommender replenish with a new document
+    }
+}
