15db1621feafd16da4a022d60e17c0f6edc56705
Dylan Leclair <dylan.leclair1@ucalgary.ca>
2021-02-18 21:04:54 -0700

    Fixed MathNET setup to work properly

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,16 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
+
 using UnityEngine;
+
+
 
 public class Agent : MonoBehaviour
 {
+    /// <summary>
+    /// A reference to the recommender?
+    /// </summary>
+    Recommender r;
+
+    /// <summary>
+    /// Should have some level of interest in categories. I was thinking these should all start around 5 on a scale of 1-10? 
+    /// This way, the agents will have at least some bearing without starting them off with an already extreme profile.
+    /// </summary>
+    public List<int> Scores { get; set; }
+
     void Start()
     {
+        // Creates the system parameters
+        r = GetComponent<Recommender>();
+
+        // Generate random set of interests
+
+
+        // populate the agent's ratings
+        // generates random values from 0-10 for now
+        // @TODO: change for later!
+        Scores = new List<int>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Scores.Add(Random.Range(0, 10));
+        }
+
         
+        string log = "";
+        foreach (var item in Scores)
+        {
+            log+=item.ToString() + " ";
+        }
+        Debug.Log("Agent initialized with preferences: " + log);
+
+
     }
 
     void Update()
     {
         
     }
+
+
+
 }
--- Assets/Scripts/Recommender.cs
+++ Assets/Scripts/Recommender.cs
@@ -1,18 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.Statistics;
+using MathNet.Numerics;
 using UnityEngine;
+
+
+
+
+public class Parameters
+{
+    /// <summary>
+    /// Controls the number of agents in the system
+    /// </summary>
+    public int NumberAgents { get; set; }
+    /// <summary>
+    /// Controls the number of documents in the system
+    /// </summary>
+    public int NumberDocuments { get; set; }
+    /// <summary>
+    /// Controls how many like users the algorithm identifies to make a recommendation
+    /// </summary>
+    public int LikeAgentNum { get; set; }
+
+    /// <summary>
+    /// The type of metric that is used to filter
+    /// </summary>
+    public Metric metric { get; set; }
+
+    /// <summary>
+    /// Sets the system parameters according to the given arguments.
+    /// </summary>
+    /// <param name="agents">the number of agents in the system</param>
+    /// <param name="docs">the number of documents in the system</param>
+    public Parameters(int agents, int docs)
+    {
+        NumberAgents = agents;
+        NumberDocuments = docs;
+        metric = Metric.Cosine;
+    }
+
+}
+
+
+public enum Metric
+{
+    Cosine,
+    CorrectedCosine,
+    Pearson,
+    Euclidean
+}
+
 
 public class Recommender : MonoBehaviour
 {
+
+    public Parameters sysp { get; set; }
+
+    List<Agent> Agents = new List<Agent>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        sysp = new Parameters(10, 10);
+
+        // get a list of the agents somehow
+
+        for (int i = 0; i < sysp.NumberAgents; i++)
+        {
+            Agents.Add(new Agent());
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // Recalculate the matrix
+
         
     }
 }
